741a62ccf9d243f1dd430f97454d0ec4a7bb12ad
Juliszelu <juliszelubski@MacBook-Pro-de-Juli.local>
2024-11-18 17:06:13 -0300

    Boton cargar mas

--- js/index.js
+++ js/index.js
@@ -1,29 +1,38 @@
 const listaRecetas = document.querySelector('.lista-recetas');
 let recetas = '';
+let skip = 0;
+const limit = 10;
 
-fetch("https://dummyjson.com/recipes")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data.recipes);
-    
-    for (let i = 0; i < data.recipes.length; i++) {
-      const receta = data.recipes[i];
-      const markup = `
-        <article class='receta'>
-          <img src="${receta.image}" alt="${receta.name}">
-          <h2>${receta.name}</h2>
-          <p>Dificultad: ${receta.difficulty}</p>
-          <a href="receta.html?id=${receta.id}">Ver detalle</a>
-        </article>
-      `;
-      recetas += markup;
-    }
-    listaRecetas.innerHTML = recetas;
-  })
-  .catch(function(error) {
-    console.log("Error: " + error);
-  });
+function cargarRecetas() {
+    fetch(`https://dummyjson.com/recipes?skip=${skip}&limit=${limit}`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data.recipes);
 
-  
+            for (let i = 0; i < data.recipes.length; i++) {
+                const receta = data.recipes[i];
+                const markup = `
+                    <article class='receta'>
+                        <img src="${receta.image}" alt="${receta.name}">
+                        <h2>${receta.name}</h2>
+                        <p>Dificultad: ${receta.difficulty}</p>
+                        <a href="receta.html?id=${receta.id}">Ver detalle</a>
+                    </article>
+                `;
+                recetas += markup;
+            }
+            listaRecetas.innerHTML = recetas;
+            skip += limit; // Mueve esto dentro del bloque .then
+        })
+        .catch(function(error) {
+            console.log("Error: " + error);
+        });
+}
+
+// Evento para cargar más recetas
+document.getElementById('cargar-mas').addEventListener('click', cargarRecetas);
+
+// Cargar las recetas iniciales
+cargarRecetas();
