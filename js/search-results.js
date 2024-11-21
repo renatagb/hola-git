793bd2c52810336af1008c31e74d3879068abb2f
Juliszelu <juliszelubski@MacBook-Pro-de-Juli.local>
2024-11-21 17:06:01 -0300

    Agregue en el buscador que no puede estar vacio y no puede tener menos de 3 caracteres

--- js/search-results.js
+++ js/search-results.js
@@ -1,36 +1,45 @@
+
 const queryString = location.search; 
 const queryParams = new URLSearchParams(queryString); 
 const resultados2 = queryParams.get('q'); 
 
-fetch(`https://dummyjson.com/recipes/search?q=${resultados2}`)
-.then(function (response) {
-    return response.json();
-})
-.then(function(data){
-    console.log(data);
-    const results = document.querySelector('.resultado-busqueda');
-    let resu = "";
+const results = document.querySelector('.resultado-busqueda');
 
-    resu += `<h1>Resultados de búsqueda para: ${resultados2}</h1>`;
+if (resultados2 === '') {
+    results.innerHTML = `<p class="textobusca">El campo de búsqueda no puede estar vacío.</p>`;
+} else if (resultados2.length < 3) {
+    results.innerHTML = `<p class="textobusca">El término de búsqueda debe tener al menos 3 caracteres.</p>`;
+} else {
+    fetch(`https://dummyjson.com/recipes/search?q=${resultados2}`)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        let resu = "";
 
-    if (data.recipes && data.recipes.length > 0) {
-        for (let i = 0; i < data.recipes.length; i++) {
-            const receta = data.recipes[i];
-            const markup = `
-                <article class='receta'>
-                    <img src="${receta.image}" alt="${receta.name}">
-                    <h2>${receta.name}</h2>
-                    <a href="receta.html?id=${receta.id}">Ver detalle</a>
-                </article>
-            `;
-            resu += markup;
+        resu += `<h1>Resultados de búsqueda para: ${resultados2}</h1>`;
+
+        if (data.recipes && data.recipes.length > 0) {
+            for (let i = 0; i < data.recipes.length; i++) {
+                const receta = data.recipes[i];
+                const markup = `
+                    <article class='receta'>
+                        <img src="${receta.image}" alt="${receta.name}">
+                        <h2>${receta.name}</h2>
+                        <a href="receta.html?id=${receta.id}">Ver detalle</a>
+                    </article>
+                `;
+                resu += markup;
+            }
+        } else {
+            resu += `<p class="textobusca">No se encontraron resultados para: ${resultados2}</p>`;
         }
-    } else {
-        resu = `<p>No se encontraron resultados para: ${resultados2}</p>`;
-    }
 
-    results.innerHTML = resu;
-})
-.catch(function (error) {
-    console.log("Error: " + error);
-});
+        results.innerHTML = resu;
+    })
+    .catch(function (error) {
+        console.log("Error: " + error);
+        results.innerHTML = `<p style="color: red;">Ocurrió un error al buscar las recetas.</p>`;
+    });
+}
