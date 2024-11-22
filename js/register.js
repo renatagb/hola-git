ff38ed8b260912af011605b847631d371d2af4a9
morasternn <mstern@udesa.edu.ar>
2024-11-22 10:31:45 -0300

    Register.html y register.js

---
+++ js/register.js
@@ -0,0 +1,38 @@
+const form = document.querySelector('form')
+
+
+//Campos
+
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+
+//Invalid texts
+const invalidEmail = document.querySelector('.email')
+const invalidPassword = document.querySelector('.password')
+
+//Crear event listener submit para el form
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    let error= false;
+    if(email.value === ''){
+        invalidEmail.innerText = 'Por favor complete este campo';
+        invalidEmail.style.display = 'block';
+        error=true;
+    }else{
+        invalidEmail.style.display='none';
+    }
+    if(password.value === '' ){
+        invalidPassword.innerText = 'Por favor complete este campo';
+        invalidPassword.style.display = 'block';
+        error=true;
+    }else{
+        invalidPassword.style.display='none';
+    }
+
+   
+    if(!error){
+        this.submit();
+    }
+       
+})
+
